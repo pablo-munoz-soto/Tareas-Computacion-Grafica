f4bb2c3403f65ea68120a2720de3435a36be23a6
pablo muñoz <pmunoz1999@gmail.com>
2020-05-21 15:46:20 -0400

    implementacion de rounded non uniform spline y dibujo de la pista

--- tarea2b/crazy-racer.py
+++ tarea2b/crazy-racer.py
@@ -17,35 +17,102 @@ import modulos.lighting_shaders as ls
 import modulos.scene_graph as sg
 import modulos.ex_curves as cu
 
+def normalizar(vector):
+    modulo=(vector[0][0]**2+vector[1][0]**2+vector[2][0]**2)**(1/2)
+    if modulo==0:return
+    vector=np.multiply(vector,1/modulo)
+    return vector
 
-#puntos=[np.array([[-0.5,-0.5,0]]).T,np.array([[0.5,-0.5,0]]).T,np.array([[0.5,0.5,0]]).T,np.array([[-0.5,0.5,0]]).T]
-puntos=[np.array([[-0.7,-0.7,0]]).T,np.array([[0.5,-0.7,0]]).T,np.array([[0.5,-0.5,0]]).T,np.array([[0.7,0.3,0]]).T,np.array([[0.3,0.7,0]]).T,np.array([[0.2,0.3,0]]).T,np.array([[0.2,0.1,0]]).T,np.array([[-0.2,0.1,0]]).T,np.array([[-0.5,0.7,0]]).T,np.array([[-0.7,0.3,0]]).T,np.array([[-0.8,-0.6,0]]).T]
+puntos=[np.array([[-0.5,-1,0]]).T,np.array([[0,-2,0]]).T,np.array([[0.5,-1,0.3]]).T,np.array([[0,0,0]]).T,np.array([[0.5,1,0]]).T,np.array([[0,1.5,0.2]]).T,np.array([[-0.5,1,0]]).T,np.array([[-1.7,0,0]]).T,np.array([[-1,-0.3,0]]).T]
+
+
+
+
+
+
+def velocidad(P0,P1,P2):
+    a=((P0[0][0] - P2[0][0]) ** 2 + (P0[1][0] - P2[1][0]) ** 2) ** (1 / 2)
+    b = ((P0[0][0] - P1[0][0]) ** 2 + (P0[1][0] - P1[1][0]) ** 2 ) ** (1 / 2)
+    c = ((P2[0][0] - P1[0][0]) ** 2 + (P2[1][0] - P1[1][0]) ** 2 ) ** (1 / 2)
+    traslacion=P0
+    P2=np.subtract(P2,P1)
+    z2=P2[2][0]
+    P2[2][0]=0
+    P0=np.subtract(P0,P1)
+    z0=P0[2][0]
+    P0[2][0]=0
+    P1=np.array([[0,0,0]]).T
+
+
+    alfa=np.arccos((b**2+c**2-a**2)/(2*b*c))
+    if -0.001<alfa-np.pi<0.001:
+        velocidad=normalizar(np.array([[P2[0][0]-P0[0][0],P2[1][0]-P0[1][0],0]]).T)
+        velocidad[2][0]=(z2-z0)/2
+        return velocidad
+    alfa/=2
+    rotacion=np.array([[np.cos(alfa),-np.sin(alfa),0],[np.sin(alfa),np.cos(alfa),0],[0,0,1]])
+    bisectriz=np.matmul(rotacion,P2)
+    x=P2[0][0]
+    if x==0:
+        if traslacion[0][0]>0:x-=0.1
+        else:x+=0.1
+    if bisectriz[0][0]==0 :
+        y=0
+    if bisectriz[1][0]==0:
+     y=P2[1][0]
+     x=0
+    if bisectriz[0][0]!=0 and bisectriz[1][0]!=0:
+        y = (-bisectriz[0][0] * x) / bisectriz[1][0]
+    velocidad=np.array([[x,y,(z2-z0)/2]]).T
+    if (P2[0][0]-P0[0][0])*velocidad[0][0]<0:velocidad[0][0]*=-1
+    if (P2[1][0] - P0[1][0]) * velocidad[1][0] < 0: velocidad[1][0] *= -1
+    return normalizar(velocidad)
+
+
+
 
 def crearSubcurvas(puntos):
  subcurvas=[]
  for i in range(len(puntos)):
     if i==0:
-        Gmb = cu.spliceMatrix(puntos[len(puntos)-1], puntos[i], puntos[i + 1], puntos[i + 2])
+        P1=puntos[i]
+        P2=puntos[i+1]
+        v1=velocidad(puntos[len(puntos)-1],P1,P2)
+        v2=velocidad(P1,P2,puntos[i+2])
+        Gmb = cu.hermiteMatrix(P1, P2, v1, v2)
         # funcion que genera la spline de catmull-rom
         distancia=((puntos[i][0][0]-puntos[i+1][0][0])**2+(puntos[i][1][0]-puntos[i+1][1][0])**2+(puntos[i][2][0]-puntos[i+1][2][0])**2)**(1/2)
         splicerCurve = cu.evalCurve(Gmb, int(distancia*500))
         subcurvas += [splicerCurve]
+
     elif i==len(puntos)-2:
-        Gmb = cu.spliceMatrix(puntos[i-1], puntos[i], puntos[i + 1], puntos[0])
+        P1 = puntos[i]
+        P2 = puntos[i + 1]
+        v1 = velocidad(puntos[i-1], P1, P2)
+        v2 = velocidad(P1, P2, puntos[0])
+        Gmb = cu.hermiteMatrix(P1, P2, v1, v2)
         # funcion que genera la spline de catmull-rom
         distancia=((puntos[i][0][0]-puntos[i+1][0][0])**2+(puntos[i][1][0]-puntos[i+1][1][0])**2+(puntos[i][2][0]-puntos[i+1][2][0])**2)**(1/2)
         splicerCurve = cu.evalCurve(Gmb, int(distancia*500))
         subcurvas += [splicerCurve]
 
     elif i==len(puntos)-1:
-        Gmb = cu.spliceMatrix(puntos[i-1], puntos[i], puntos[0], puntos[1])
+        P1 = puntos[i]
+        P2 = puntos[0]
+        v1 = velocidad(puntos[i - 1], P1, P2)
+        v2 = velocidad(P1, P2, puntos[1])
+        Gmb = cu.hermiteMatrix(P1, P2, v1, v2)
         # funcion que genera la spline de catmull-rom
         distancia = ((puntos[i][0][0] - puntos[0][0][0]) ** 2 + (puntos[i][1][0] - puntos[0][1][0]) ** 2 + (puntos[i][2][0] - puntos[0][2][0]) ** 2) ** (1 / 2)
         splicerCurve = cu.evalCurve(Gmb, int(distancia*500))
         subcurvas += [splicerCurve]
 
     else:
-        Gmb = cu.spliceMatrix(puntos[i - 1], puntos[i], puntos[i + 1], puntos[i + 2])
+        P1 = puntos[i]
+        P2 = puntos[i+1]
+        v1 = velocidad(puntos[i - 1], P1, P2)
+        v2 = velocidad(P1, P2, puntos[i+2])
+        Gmb = cu.hermiteMatrix(P1, P2, v1, v2)
         # funcion que genera la spline de catmull-rom
         distancia = ((puntos[i][0][0] - puntos[i + 1][0][0]) ** 2 + (puntos[i][1][0] - puntos[i + 1][1][0]) ** 2 + ( puntos[i][2][0] - puntos[i + 1][2][0]) ** 2) ** (1 / 2)
         splicerCurve = cu.evalCurve(Gmb, int(distancia*500))
@@ -58,14 +125,16 @@ def crearSubcurvas(puntos):
      curvas.childs += [n]
  return curvas
 
+
+
 def crear_auto():
     #creando la parte trasera
     vertices = [
-        #   positions        texture
-        -0.5, -0.3, 0.0, 0.045, 0.3,
-        0.5, -0.3, 0.0, 0.4425, 0.3,
-        0.5, 0.3, 0.0, 0.4425, 0.2,
-        -0.5, 0.3, 0.0, 0.045, 0.2]
+        #   positions        texture    normal
+        -0.5, -0.3, 0.0, 0.045, 0.3,    0,0,1,
+        0.5, -0.3, 0.0, 0.4425, 0.3,    0,0,1,
+        0.5, 0.3, 0.0, 0.4425, 0.2,   0,0,1,
+        -0.5, 0.3, 0.0, 0.045, 0.2,    0,0,1]
     indices = [
         0, 1, 2,
         2, 3, 0]
@@ -74,75 +143,94 @@ def crear_auto():
     rectanguloTrasero.childs+=[es.toGPUShape(bs.Shape(vertices, indices, textureFileName),GL_REPEAT, GL_NEAREST)]
 
     vertices = [
-        #   positions        texture
-        -0.5, -0.3, 0.0, 0.045, 0.2,
-        0.5, -0.3, 0.0, 0.4425, 0.2,
-        0.5, 0.1, 0.0, 0.42, 0.1,
-        -0.5, 0.1, 0.0, 0.09, 0.1]
+        #   positions        texture    normal
+        -0.5, -0.3, 0.0, 0.045, 0.2,    0,0,1,
+        0.5, -0.3, 0.0, 0.4425, 0.2,    0,0,1,
+        0.5, 0.1, 0.0, 0.42, 0.1,    0,0,1,
+        -0.5, 0.1, 0.0, 0.09, 0.1,    0,0,1,]
     indices = [
         0, 1, 2,
         2, 3, 0]
     ventanaTrasera=sg.SceneGraphNode('ventanaTrasera')
     ventanaTrasera.childs+=[es.toGPUShape(bs.Shape(vertices, indices, textureFileName),GL_REPEAT, GL_NEAREST)]
-    ventanaTrasera.transform=tr.translate(0,0.5,0)
+    ventanaTrasera.transform=tr.translate(0,0.6,0)
     atras=sg.SceneGraphNode('atras')
     atras.childs+=[ventanaTrasera,rectanguloTrasero]
     atras.transform=tr.rotationX(np.pi/2)
 
     #creando la parte lateral
     vertices = [
-        #   positions        texture
-        -0.7, -0.3, 0.0, 0.035, 0.96,
-        0.7, -0.3, 0.0, 0.79, 0.96,
-        0.7, 0.3, 0.0, 0.79, 0.8,
-        -0.7, 0.3, 0.0, 0.035, 0.8]
+        #   positions        texture     normal
+        -0.7, -0.3, 0.0, 0.035, 0.96,    0,0,-1,
+        0.7, -0.3, 0.0, 0.79, 0.96,    0,0,-1,
+        0.7, 0.3, 0.0, 0.79, 0.8,    0,0,-1,
+        -0.7, 0.3, 0.0, 0.035, 0.8,    0,0,-1,]
     indices = [
         0, 1, 2,
         2, 3, 0]
     autoLateral1 = sg.SceneGraphNode('autoLateral1')
     autoLateral1.childs += [es.toGPUShape(bs.Shape(vertices, indices, textureFileName), GL_REPEAT, GL_NEAREST)]
-    autoLateral1.transform = np.matmul(np.matmul(tr.translate(-0.5, 0.7, 0),tr.rotationX(np.pi/2)),tr.rotationY(np.pi/2))
+    autoLateral1.transform = np.matmul(np.matmul(tr.translate(-0.5, 0.7, 0.05),tr.rotationX(np.pi/2)),tr.rotationY(np.pi/2))
     autoLateral2 = sg.SceneGraphNode('autoLateral2')
     autoLateral2.childs += [es.toGPUShape(bs.Shape(vertices, indices, textureFileName), GL_REPEAT, GL_NEAREST)]
-    autoLateral2.transform = np.matmul(np.matmul(tr.translate(0.5, 0.7, 0), tr.rotationX(np.pi / 2)), tr.rotationY(np.pi / 2))
+    autoLateral2.transform = np.matmul(np.matmul(tr.translate(0.5, 0.7, 0.05), tr.rotationX(np.pi / 2)), tr.rotationY(np.pi / 2))
 
     vertices = [
-        #   positions        texture
-        -0.7, -0.15, 0.0, 0.15, 0.78,
-        0.7, -0.15, 0.0, 0.47, 0.78,
-        0.7, 0.15, 0.0, 0.47, 0.71,
-        -0.7, 0.15, 0.0, 0.15, 0.71]
+        #   positions        texture     normal
+        -0.7, -0.15, 0.0, 0.15, 0.78,    0,0,-1,
+        0.7, -0.15, 0.0, 0.47, 0.78,    0,0,-1,
+        0.7, 0.15, 0.0, 0.47, 0.71,    0,0,-1,
+        -0.7, 0.15, 0.0, 0.15, 0.71,    0,0,-1,]
     indices = [
         0, 1, 2,
         2, 3, 0]
     ventanaLateral1 = sg.SceneGraphNode('ventanaLateral1')
     ventanaLateral1.childs += [es.toGPUShape(bs.Shape(vertices, indices, textureFileName), GL_REPEAT, GL_NEAREST)]
-    ventanaLateral1.transform = np.matmul(np.matmul(tr.translate(0.5, 0.7, 0.45), tr.rotationX(np.pi / 2)),tr.rotationY(np.pi / 2))
+    ventanaLateral1.transform = np.matmul(np.matmul(tr.translate(0.5, 0.7, 0.5), tr.rotationX(np.pi / 2)),tr.rotationY(np.pi / 2))
 
     ventanaLateral2 = sg.SceneGraphNode('ventanaLateral2')
     ventanaLateral2.childs += [es.toGPUShape(bs.Shape(vertices, indices, textureFileName), GL_REPEAT, GL_NEAREST)]
-    ventanaLateral2.transform = np.matmul(np.matmul(tr.translate(-0.5, 0.7, 0.45), tr.rotationX(np.pi / 2)),
+    ventanaLateral2.transform = np.matmul(np.matmul(tr.translate(-0.5, 0.7, 0.5), tr.rotationX(np.pi / 2)),
                                           tr.rotationY(np.pi / 2))
     lateral=sg.SceneGraphNode('lateral')
     lateral.childs+=[autoLateral1,autoLateral2,ventanaLateral1,ventanaLateral2]
+    lateral.transform=tr.scale(1,1,1.1)
 
     #creando el techo
     vertices = [
-        #   positions        texture
-        -0.7, -0.5, 0.0, 0.28, 0.56,
-        0.7, -0.5, 0.0, 0.6, 0.56,
-        0.7, 0.5, 0.0, 0.6, 0.39,
-        -0.7, 0.5, 0.0, 0.28, 0.39]
+        #   positions        texture    normal
+        -0.7, -0.5, 0.0, 0.28, 0.56,    0,0,1,
+        0.7, -0.5, 0.0, 0.6, 0.56,    0,0,1,
+        0.7, 0.5, 0.0, 0.6, 0.39,    0,0,1,
+        -0.7, 0.5, 0.0, 0.28, 0.39,    0,0,1,]
     indices = [
         0, 1, 2,
         2, 3, 0]
     techo = sg.SceneGraphNode('techo')
     techo.childs += [es.toGPUShape(bs.Shape(vertices, indices, textureFileName), GL_REPEAT, GL_NEAREST)]
-    techo.transform = np.matmul(tr.translate(0, 0.7, 0.6),tr.rotationZ(np.pi/2))
+    techo.transform = np.matmul(tr.translate(0, 0.7, 0.7),tr.rotationZ(np.pi/2))
+
+    vertices = [
+        #   positions        texture    normal
+        -0.3, -0.3, 0.0, 0.68, 0.99,    0,0,-1,
+        0.3, -0.3, 0.0, 0.78, 0.99,    0,0,-1,
+        0.3, 0.3, 0.0, 0.78, 0.88,    0,0,-1,
+        -0.3, 0.3, 0.0, 0.68, 0.88,    0,0,-1,]
+    indices = [
+        0, 1, 2,
+        2, 3, 0]
+    ruedatrasera1 = sg.SceneGraphNode('ruedatrasera1')
+    ruedatrasera1.childs += [es.toGPUShape(bs.Shape(vertices, indices, textureFileName), GL_REPEAT, GL_NEAREST)]
+    ruedatrasera1.transform = np.matmul(np.matmul(tr.translate(0.51, 0.25, -0.1),tr.rotationY(np.pi/2)),tr.uniformScale(0.65))
+
+    ruedatrasera2 = sg.SceneGraphNode('ruedatrasera2')
+    ruedatrasera2.childs += [es.toGPUShape(bs.Shape(vertices, indices, textureFileName), GL_REPEAT, GL_NEAREST)]
+    ruedatrasera2.transform = np.matmul(np.matmul(tr.translate(-0.51, 0.25, -0.1), tr.rotationY(np.pi / 2)),
+                                        tr.uniformScale(0.65))
 
 
     auto = sg.SceneGraphNode('auto')
-    auto.childs+=[atras,lateral,techo]
+    auto.childs+=[atras,lateral,techo,ruedatrasera1,ruedatrasera2]
     return auto
 
 
@@ -177,9 +265,8 @@ if __name__ == "__main__":
 
     # Different shader programs for different lighting strategies
 
-    #lightingPipeline=es.SimpleModelViewProjectionShaderProgram()
-    lightingPipeline=es.SimpleTextureModelViewProjectionShaderProgram()
-    #lightingPipeline = ls.SimpleTexturePhongShaderProgram()
+    lightingPipeline=es.SimpleModelViewProjectionShaderProgram()
+    lightingTexturePipeline= ls.SimpleTextureGouraudShaderProgram()
 
     # Setting up the clear screen color
     glClearColor(0.85, 0.85, 0.85, 1.0)
@@ -266,14 +353,41 @@ if __name__ == "__main__":
         glUniformMatrix4fv(glGetUniformLocation(lightingPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
         glUniformMatrix4fv(glGetUniformLocation(lightingPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
 
-        gpuShape=es.toGPUShape(bs.createColorNormalsCube(0,1,0))
-        nodo=sg.SceneGraphNode('nodo')
-        nodo.childs+=[gpuShape]
 
-        #gpu=crearSubcurvas(puntos)
+
         # Drawing
-        #sg.drawSceneGraphNode(gpu,lightingPipeline,'model',GL_LINE_STRIP)
-        sg.drawSceneGraphNode(auto, lightingPipeline, 'model')
+        sg.drawSceneGraphNode(gpu,lightingPipeline,'model',GL_LINE_STRIP)
+
+
+
+
+        glUseProgram(lightingTexturePipeline.shaderProgram)
+
+        # White light in all components: ambient, diffuse and specular.
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
+
+        # Object is barely visible at only ambient. Diffuse behavior is slightly red. Sparkles are white
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "Ka"), 0.2, 0.2, 0.2)
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "Kd"), 0.9, 0.5, 0.5)
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "Ks"), 1.0, 1.0, 1.0)
+
+        # TO DO: Explore different parameter combinations to understand their effect!
+
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "lightPosition"), -5, -5, 5)
+        glUniform3f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "viewPosition"), viewPos[0], viewPos[1],
+                    viewPos[2])
+        glUniform1ui(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "shininess"), 100)
+
+        glUniform1f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "constantAttenuation"), 0.0001)
+        glUniform1f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "linearAttenuation"), 0.03)
+        glUniform1f(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "quadraticAttenuation"), 0.01)
+
+        glUniformMatrix4fv(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+        glUniformMatrix4fv(glGetUniformLocation(lightingTexturePipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+
+        #sg.drawSceneGraphNode(auto, lightingTexturePipeline, 'model')
         
         # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
         glfw.swap_buffers(window)
